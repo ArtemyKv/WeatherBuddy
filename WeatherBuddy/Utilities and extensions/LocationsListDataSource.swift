a09002a56bd0dbea54cb9f766dbdaf9f1d5c2b96
ArtemyKv <artemykv@gmail.com>
2022-10-19 06:18:34 +0500

    add titles to locations list sections

--- WeatherBuddy/Utilities and extensions/LocationsListDataSource.swift
+++ WeatherBuddy/Utilities and extensions/LocationsListDataSource.swift
@@ -27,4 +27,12 @@ class LocationsListDataSource: UITableViewDiffableDataSource<LocationsListViewMo
         }
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return "Current location"
+        } else {
+            return "Favorite locations"
+        }
+    }
 }
